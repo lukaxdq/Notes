908ad9ca325b4c4cd55dccb2323824e55f8f44c2
lukaxdq <117482951+lukaxdq@users.noreply.github.com>
2025-02-03 22:31:06 +0100

    Changes to be committed: 	new file:   .vscode/tasks.json 	modified:   src/Note.cpp

--- src/Note.cpp
+++ src/Note.cpp
@@ -1,20 +1,4 @@
-#ifndef NOTE_H
-#define NOTE_H
 #include "Note.h"
-#include <string>
-
-class Note {
-public:
-    Note();
-    Note(const std::string& content);
-    std::string getContent() const;
-    void setContent(const std::string& content);
-
-private:
-    std::string content;
-};
-
-#endif // NOTE_H
 
 Note::Note() : content("") {}
 
